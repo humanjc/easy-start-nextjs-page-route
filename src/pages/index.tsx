7514eee677c60684f2148cd17da66dc57882df0f
humanjc <humanjc@daum.net>
2026-06-22 13:45:07 +0900

    index.tsx static param

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
 import Link from "next/link";
+import { GetServerSideProps } from "next";
 
-export default function Home() {
+type PageProps = {
+  data?: {
+    title?: string;
+    msg?: string;
+  };
+};
+
+export const getServerSideProps: GetServerSideProps<PageProps> = async () => {
+  const data = {
+    title: "Index page",
+    msg: "시작페이지입니다.",
+  };
+  return { props: { data } };
+};
+
+export default function Home({ data }: PageProps) {
   return (
     <main>
-      <p>This is sample page.</p>
+      <p>{data?.msg ?? ""}</p>
       <div>
         <Link href="/other">Go &quot;Other&quot;.</Link>
       </div>
