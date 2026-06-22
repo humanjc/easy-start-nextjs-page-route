16da4370265089bc946d9e3196f4f9abf55ce298
humanjc <humanjc@daum.net>
2026-06-22 19:43:00 +0900

    getStaticPaths

--- src/pages/name/[name].tsx
+++ src/pages/name/[name].tsx
@@ -1,17 +1,67 @@
 import Link from "next/link";
+import type { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
 
-export default function Name() {
+type PageProps = {
+  data: {
+    title: string;
+    msg: string;
+  };
+};
+
+type Params = {
+  name: "kim" | "lee" | "park";
+};
+
+export default function Name({ data }: PageProps) {
   const router = useRouter();
   return (
     <main>
-      <h1>Name page.</h1>
-      <p>
-        Your name: <b>&quot;{router.query.name}&quot;</b>.
-      </p>
+      <h1 className="header">{data.title}</h1>
+      <p>name: {router.query.name}</p>
+      <p>message: {data.msg}</p>
       <div>
         <Link href="/">Go Back!!</Link>
       </div>
     </main>
   );
 }
+
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
+  const path = ["/name/kim", "/name/lee", "/name/park"];
+  return {
+    paths: path,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<PageProps, Params> = async ({
+  params,
+}) => {
+  if (!params) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const data: Record<Params["name"], PageProps["data"]> = {
+    kim: {
+      title: "KIM-web",
+      msg: "This is Kim's web site.",
+    },
+    lee: {
+      title: "Lee의 방",
+      msg: "여기는 Lee의 방입니다.",
+    },
+    park: {
+      title: "Park의 페이지",
+      msg: "안녕! Park의 페이지입니다!",
+    },
+  };
+
+  return {
+    props: {
+      data: data[params.name],
+    },
+  };
+};
